44204de2e4322ff48e243617819db8ece50edd1e
lapptomi <lappetelainen.tomi@gmail.com>
2023-11-25 20:42:57 +0200

    test

--- next.config.js
+++ next.config.js
@@ -5,9 +5,17 @@ const nextConfig = {
   },
   images: {
     // Allow loading images only from S3 when using Next.js <Image> component
-    domains: [`${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_BUCKET_REGION}.amazonaws.com`],
+    remotePatterns: [
+      {
+        // Allow loading images from a domain that starts with "https://example.com"
+        protocol: 'https',
+        hostname: '**'
+        // hostname: `${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_BUCKET_REGION}.amazonaws.com`,
+
+      }
+    ],
     // Cache images for 1 minute
-    minimumCacheTTL: 60
+    // minimumCacheTTL: 60
   }
 };
 
