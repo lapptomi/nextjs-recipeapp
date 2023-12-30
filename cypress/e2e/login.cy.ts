8ca496e838efc5615304e12e2c4ab0b751537206
lapptomi <lappetelainen.tomi@gmail.com>
2023-12-31 01:21:27 +0200

    fixed tests

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -15,11 +15,9 @@ describe('Login', () => {
 
   it('Displays error message on invalid login', () => {
     cy.get('input[name="email"]').type('invalidemail');
-    cy.wait(500);
     cy.get('input[name="email"]').should('have.value', 'invalidemail');
 
     cy.get('input[name="password"]').type('invalid');
-    cy.wait(500);
     cy.get('input[name="password"]').should('have.value', 'invalid');
 
     cy.get('button').contains('Sign In').click();
@@ -29,16 +27,13 @@ describe('Login', () => {
 
   it('Redirects to recipes on valid login', () => {
     cy.get('input[name="email"]').type(user.email);
-    cy.wait(500);
     cy.get('input[name="email"]').should('have.value', user.email);
 
     cy.get('input[name="password"]').type(user.password);
-    cy.wait(500);
     cy.get('input[name="password"]').should('have.value', user.password);
 
     cy.get('button').contains('Sign In').click();
 
-    cy.wait(1000);
     cy.url().should('include', '/recipes');
   });
 
--- cypress/e2e/register.cy.ts
+++ cypress/e2e/register.cy.ts
@@ -14,47 +14,37 @@ describe('Creating new user', () => {
 
   it('Redirects to /recipes page after successful registration', () => {
     cy.get('input[name="email"]').type(user.email);
-    cy.wait(500);
     cy.get('input[name="email"]').should('have.value', user.email);
 
     cy.get('input[name="username"]').type(user.username);
-    cy.wait(500);
     cy.get('input[name="username"]').should('have.value', user.username);
 
     cy.get('input[name="password"]').type(user.password);
-    cy.wait(500);
     cy.get('input[name="password"]').should('have.value', user.password);
 
     cy.get('input[name="confirmPassword"]').type(user.password);
-    cy.wait(500);
     cy.get('input[name="confirmPassword"]').should('have.value', user.password);
 
     cy.get('button').contains('Sign Up').click();
 
-    cy.wait(1000);
     cy.url().should('include', '/recipes');
   });
 
   it('Displays error message on invalid email', () => {
     cy.get('input[name="email"]').type('invalidemail');
-    cy.wait(500);
     cy.get('input[name="email"]').should('have.value', 'invalidemail');
 
     cy.get('input[name="username"]').type(user.username);
-    cy.wait(500);
     cy.get('input[name="username"]').should('have.value', user.username);
 
     cy.get('input[name="password"]').type(user.password);
-    cy.wait(500);
     cy.get('input[name="password"]').should('have.value', user.password);
 
     cy.get('input[name="confirmPassword"]').type(user.password);
-    cy.wait(500);
     cy.get('input[name="confirmPassword"]').should('have.value', user.password);
 
     cy.get('button').contains('Sign Up').click();
 
-    cy.wait(1000);
     cy.url().should('include', '/auth/register');
   });
 
