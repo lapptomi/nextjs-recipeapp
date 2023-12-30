c9b551bdcce2ba65546979ee10932bd1c58ad5f7
lapptomi <lappetelainen.tomi@gmail.com>
2023-12-30 21:55:15 +0200

    added some cypress tests

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -4,6 +4,7 @@ import { user } from "./data";
 
 describe('Login', () => {
   beforeEach(() => {
+    /*
     cy.request('DELETE', 'http://localhost:3000/api/users')
       .then(() => {
         cy.request('POST', 'http://localhost:3000/api/users', user);
@@ -12,6 +13,10 @@ describe('Login', () => {
         cy.wait(1000);
         cy.visit("http://localhost:3000/auth/login");
       });
+    */
+    cy.request('DELETE', 'http://localhost:3000/api/users');
+    cy.request('POST', 'http://localhost:3000/api/users', user);
+    return cy.visit("http://localhost:3000/auth/login");
   });
 
   it('Displays login form', () => {
@@ -22,9 +27,11 @@ describe('Login', () => {
 
   it('Displays error message on invalid login', () => {
     cy.get('input[name="email"]').type('invalidemail');
+    cy.wait(500);
     cy.get('input[name="email"]').should('have.value', 'invalidemail');
 
     cy.get('input[name="password"]').type('invalid');
+    cy.wait(500);
     cy.get('input[name="password"]').should('have.value', 'invalid');
 
     cy.get('button').contains('Sign In').click();
@@ -34,14 +41,16 @@ describe('Login', () => {
 
   it('Redirects to recipes on valid login', () => {
     cy.get('input[name="email"]').type(user.email);
+    cy.wait(500);
     cy.get('input[name="email"]').should('have.value', user.email);
 
     cy.get('input[name="password"]').type(user.password);
+    cy.wait(500);
     cy.get('input[name="password"]').should('have.value', user.password);
 
     cy.get('button').contains('Sign In').click();
 
-    cy.wait(500);
+    cy.wait(1000);
     cy.url().should('include', '/recipes');
   });
 
--- cypress/e2e/register.cy.ts
+++ cypress/e2e/register.cy.ts
@@ -4,10 +4,8 @@ import { user } from "./data";
 
 describe('Creating new user', () => {
   beforeEach(() => {
-    cy.request('DELETE', 'http://localhost:3000/api/users')
-      .then(() => {
-        cy.visit("http://localhost:3000/auth/register");
-      });
+    cy.request('DELETE', 'http://localhost:3000/api/users');
+    return cy.visit("http://localhost:3000/auth/register");
   });
 
   it('Displays register form', () => {
@@ -18,43 +16,48 @@ describe('Creating new user', () => {
 
   it('Redirects to /recipes page after successful registration', () => {
     cy.get('input[name="email"]').type(user.email);
+    cy.wait(500);
     cy.get('input[name="email"]').should('have.value', user.email);
 
     cy.get('input[name="username"]').type(user.username);
+    cy.wait(500);
     cy.get('input[name="username"]').should('have.value', user.username);
 
     cy.get('input[name="password"]').type(user.password);
+    cy.wait(500);
     cy.get('input[name="password"]').should('have.value', user.password);
 
     cy.get('input[name="confirmPassword"]').type(user.password);
+    cy.wait(500);
     cy.get('input[name="confirmPassword"]').should('have.value', user.password);
 
     cy.get('button').contains('Sign Up').click();
 
+    cy.wait(1000);
     cy.url().should('include', '/recipes');
   });
 
   it('Displays error message on invalid email', () => {
     cy.get('input[name="email"]').type('invalidemail');
+    cy.wait(500);
     cy.get('input[name="email"]').should('have.value', 'invalidemail');
 
     cy.get('input[name="username"]').type(user.username);
+    cy.wait(500);
     cy.get('input[name="username"]').should('have.value', user.username);
 
     cy.get('input[name="password"]').type(user.password);
+    cy.wait(500);
     cy.get('input[name="password"]').should('have.value', user.password);
 
     cy.get('input[name="confirmPassword"]').type(user.password);
+    cy.wait(500);
     cy.get('input[name="confirmPassword"]').should('have.value', user.password);
 
     cy.get('button').contains('Sign Up').click();
 
+    cy.wait(1000);
     cy.url().should('include', '/auth/register');
-  });
-
-  it('Displays error message on invalid username', () => {
-    // Cehck that 1 == 1
-    expect(1).to.equal(2);
   });
 
 });
