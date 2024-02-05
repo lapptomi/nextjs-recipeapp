556f9bd84ddd5188da9883e8a587eb3019ae65e4
lapptomi <lappetelainen.tomi@gmail.com>
2024-02-05 23:14:28 +0200

    minor changes

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -10,19 +10,21 @@ import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { AuthGuard } from './auth.guard';
 import { User } from 'src/users/entities/user.entity';
+import { JwtTokenDto } from './dto/jwt-token.dto';
+import { Request as ExpressRequest } from 'express';
 
 @Controller('/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
-  login(@Body() credentials: LoginDto): Promise<{ token: string }> {
+  login(@Body() credentials: LoginDto): Promise<JwtTokenDto> {
     return this.authService.login(credentials);
   }
 
   @Get('/me')
   @UseGuards(AuthGuard)
-  findUserByToken(@Request() request): Promise<User> {
+  findUserByToken(@Request() request: ExpressRequest): Promise<User> {
     return this.authService.getLoggedUser(request.token);
   }
 }
